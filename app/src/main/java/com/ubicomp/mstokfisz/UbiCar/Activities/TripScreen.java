a98541de5a420e1f015d395b15da6b630013db47
M.Stokfisz <maciek.stokfisz@gmail.com>
2019-01-09 02:41:42 +0100

    Added initial GUI files

---
+++ app/src/main/java/com/ubicomp/mstokfisz/UbiCar/Activities/TripScreen.java
@@ -0,0 +1,14 @@
+package com.ubicomp.mstokfisz.UbiCar.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.ubicomp.mstokfisz.UbiCar.R;
+
+public class TripScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_screen);
+    }
+}
